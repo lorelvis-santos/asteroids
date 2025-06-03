f6b7923003a961da887c848542fc9a536fc1db22
lorelvis-santos <santoslorelvis@gmail.com>
2025-06-02 21:44:27 -0400

    initializing game

---
+++ main.py
@@ -0,0 +1,25 @@
+from constants import *
+import pygame
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids Game")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+        screen.fill("#000000")  # Fill the screen with black
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
+
+
